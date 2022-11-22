f38066ae18678886198922b9315bebcb14c128ea
dell <dell@mypc>
2022-11-22 16:34:14 -0500

    com

--- src/test/java/PracticePackage/FirstTest.java
+++ src/test/java/PracticePackage/FirstTest.java
@@ -3,7 +3,7 @@ package PracticePackage;
 public class FirstTest {
 
 	public static void main(String[] args) {
-     System.out.println("FirstTest.main()");
+     
      
      
      System.out.println("Second.main()");
