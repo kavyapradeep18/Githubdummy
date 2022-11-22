1c1b0b1858a390d4dc5fa04711a60c13e179eea0
dell <dell@mypc>
2022-11-22 15:20:46 -0500

    comm

---
+++ src/test/java/PracticePackage/FirstTest.java
@@ -0,0 +1,10 @@
+package PracticePackage;
+
+public class FirstTest {
+
+	public static void main(String[] args) {
+     System.out.println("FirstTest.main()");
+     System.out.println("Second.main()");
+	}
+
+}
