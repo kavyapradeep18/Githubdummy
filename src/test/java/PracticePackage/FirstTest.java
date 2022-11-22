b17db8e82ddcc463191473244f7d583768de994a
Sridevi <Sridevi@Sridevi-MB>
2022-11-22 15:37:19 +0530

    fg

--- src/test/java/PracticePackage/FirstTest.java
+++ src/test/java/PracticePackage/FirstTest.java
@@ -5,6 +5,7 @@ public class FirstTest {
 	public static void main(String[] args) {
      System.out.println("FirstTest.main()");
      System.out.println("Second.main()");
+     System.out.println("Hi");
 	}
 
 }
